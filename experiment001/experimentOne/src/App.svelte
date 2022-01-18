521fcf87f9eac8bc25075279d03ab943e628cc0b
Magnus Karström <magnus@yesweb.se>
2022-01-18 10:37:54 +0100

    Made form component that posts and resets form

--- experiment001/experimentOne/src/App.svelte
+++ experiment001/experimentOne/src/App.svelte
@@ -3,9 +3,6 @@
   import Post from './lib/Posts.svelte';
   import Form from './lib/Form.svelte';
 
-  let author = '';
-  let post = '';
-
   let posts = data.data;
 
   let isChecked = 1;
@@ -14,7 +11,7 @@
     isChecked = checkedNum;
   }
 
-  function addPost() {
+  function addPost(author, post) {
     const _post = {
       id: posts.length + 1,
       author: author,
@@ -29,34 +26,7 @@
 </script>
 
 <main class="h-screen flex flex-col justify-center items-center space-y-4">
-  <h1>Add Post</h1>
-  <section class="space-y-4">
-    <div class="form-control">
-      <label for="author" class="label">
-        <span class="label-text">Author</span>
-      </label>
-      <input
-        bind:value={author}
-        name="author"
-        type="text"
-        placeholder="Username"
-        class="input input-primary input-bordered"
-      />
-    </div>
-    <div class="form-control">
-      <label for="post" class="label">
-        <span class="label-text">Username</span>
-      </label>
-      <textarea
-        class="textarea h-24 textarea-bordered textarea-primary"
-        bind:value={post}
-        type="text"
-        name="post"
-        placeholder="Text"
-      />
-    </div>
-    <button on:click={addPost} class="btn btn-primary w-full">Submit</button>
-  </section>
+  <Form {addPost} />
 
   <section class="overflow-auto space-y-4">
     {#each posts.reverse() as post}
